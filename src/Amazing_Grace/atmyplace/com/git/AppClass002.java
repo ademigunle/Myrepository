51bac9f6bb13767226ed3ba6f1694aef4081cd8c
Ademigunle <Potters@17>
2020-12-28 23:40:25 +0000

    Adding my two applClass

---
+++ src/Amazing_Grace/atmyplace/com/git/AppClass002.java
@@ -0,0 +1,8 @@
+package Amazing_Grace.atmyplace.com.git;
+
+public class AppClass002{
+    public void Imustbesuccessfulinmylife(){
+        System.out.println("I must be successful in my life");
+    }
+
+}
---
+++ src/Amazing_Grace/atmyplace/com/git/AppClass01.java
@@ -0,0 +1,9 @@
+package Amazing_Grace.atmyplace.com.git;
+
+public class AppClass01{
+    public void Iwillnotbeavailable(){
+        System.out.println("I will not be available");
+    }
+
+
+}
